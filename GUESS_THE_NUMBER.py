6e4c24fe01e9ce28e6045c6237e2b07f4097ac0d
Jeebanjyoti Mallik <111642123+Lifelightx@users.noreply.github.com>
2022-10-25 13:15:30 +0530

    Add files via upload

---
+++ GUESS_THE_NUMBER.py
@@ -0,0 +1,22 @@
+n = 18
+
+
+case = 9
+for i in range(case):
+    num = int(input("Enter a number: "))
+
+    
+
+    if num ==n:
+        print("Wow. 🤗 you guess the right number.")
+        break
+    elif num>15 and num<22:
+        print("you are too close to the number🧐 ")
+    elif num>25:
+        print("you entered a greater number🧐")
+    if n < num:
+        print("Enter a smaller number🧐")
+    elif n>num:
+        print("Enter a larger number🧐")
+else:
+    print("Game Over 😖...........")
